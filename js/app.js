7db5df2da499697cace8c9548ae45960fee1c24e
Collin Miller Smith <cmsmith.tx@gmail.com>
2019-07-01 12:57:13 -0500

    Interactive tabs complete

--- js/app.js
+++ js/app.js
@@ -5,12 +5,21 @@ const tabContentItems = document.querySelectorAll('.tab-content-item');
 function selectItem(e) {
     // remove border from inactive tab
     removeBorder();
+    removeShow();
     // Add border to current tab
     this.classList.add('tab-border');
+    // Grab content item from DOM
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    //Add show class to item
+    tabContentItem.classList.add('show');
 }
 
 function removeBorder() {
     tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove('show'));
 }
 
 //Listen for Tab click
