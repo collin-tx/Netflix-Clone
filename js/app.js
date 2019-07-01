510a4e9b36f8d2690a367383f524847bdba50a22
Collin Miller Smith <cmsmith.tx@gmail.com>
2019-07-01 12:45:33 -0500

    Adding js to interactive tabs

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
 const tabItems = document.querySelectorAll('.tab-item');
 const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// Selects tab content item
+function selectItem(e) {
+    // remove border from inactive tab
+    removeBorder();
+    // Add border to current tab
+    this.classList.add('tab-border');
+}
+
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+//Listen for Tab click
+tabItems.forEach(item => item.addEventListener('click', selectItem));
